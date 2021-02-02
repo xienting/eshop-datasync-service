70c022064cda3c3003270df5bc0ff0c7e0cdd40a
hexin <390003727@qq.com>
2021-02-02 23:37:43 +0800

    加一个测试接口

---
+++ src/main/java/com/roncoo/eshop/datasync/web/controller/DataSyncController.java
@@ -0,0 +1,20 @@
+package com.roncoo.eshop.datasync.web.controller;
+
+import com.roncoo.eshop.datasync.service.EshopProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DataSyncController {
+
+    @Autowired
+    private EshopProductService eshopProductService;
+
+    @RequestMapping("findBrandById")
+    @ResponseBody
+    public String findBrandById(Long id) {
+        return eshopProductService.findBrandById(id);
+    }
+}
